5095fcadd9c94b4d55d5fff9115eafa93de12ebd
henryperrien <henry.perrien@selu.edu>
2024-01-25 21:14:18 -0600

    initial database setup

---
+++ Selu383.SP24.Api/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Selu383.SP24.Api
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+
+}
+
---
+++ Selu383.SP24.Api/Hotel.cs
@@ -0,0 +1,16 @@
+﻿namespace Selu383.SP24.Api
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class HotelDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
