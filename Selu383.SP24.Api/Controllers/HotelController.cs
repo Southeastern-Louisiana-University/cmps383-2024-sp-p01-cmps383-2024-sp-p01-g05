f985aec4efb79601c9861dac52b354bbabcda628
henryperrien <henry.perrien@selu.edu>
2024-01-25 21:27:35 -0600

    starting hotel controllers get all

---
+++ Selu383.SP24.Api/Controllers/HotelController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Selu383.SP24.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/hotels")] 
+    public class HotelController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public HotelController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            var hotels = _dataContext.Hotels.
+                Select(x => new HotelDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Address = x.Address,
+                })
+                .ToList();
+
+            return Ok(hotels);
+        }
+    }
+
+}
